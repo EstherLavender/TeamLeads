aacbbf314a2d8198a09793118cd6b1d2db510bac
EstherLavender <127221085+EstherLavender@users.noreply.github.com>
2024-12-10 11:01:54 +0000

    Deploying to gh-pages from @ EstherLavender/TeamLeads@8e4cdc7a9a38e832e7bedd8c648e25e929a175ba 🚀

---
+++ assets/sign-e9758888.js
@@ -0,0 +1,1 @@
+import{v as n}from"./session-request-bbde95af.js";import"./index-2cd6b337.js";async function o(e){const{account:s,params:a}=e;return n(s,a[1]),s.signMessage({message:{raw:a[0]}})}export{o as handleSignRequest};
